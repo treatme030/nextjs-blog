a4d7c5eabc240c02551a3060de0936a8e2326112
treatme030 <treatme8192@gmail.com>
2022-07-19 18:40:01 +0900

    add Featured Posts

---
+++ components/posts/posts_grid.js
@@ -0,0 +1,14 @@
+import classes from './posts_grid.module.css';
+import PostItem from './post_item';
+
+const PostsGrid = ({ posts }) => {
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post} />
+      ))}
+    </ul>
+  );
+};
+
+export default PostsGrid;
