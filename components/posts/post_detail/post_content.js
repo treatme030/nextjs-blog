89a95a8f6ec6c4e0b4626b3eb4d8b2136cf2accb
treatme030 <treatme8192@gmail.com>
2022-08-02 16:54:52 +0900

    next Image 컴포넌트 사용하여 마크다운 이미지 오버라이드

--- components/posts/post_detail/post_content.js
+++ components/posts/post_detail/post_content.js
@@ -1,13 +1,38 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import classes from './post_content.module.css';
 import PostHeader from './post_header';
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  // 마크다운에 직접 이미지 추가하여 기존 이미지 덮어씌우기
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
