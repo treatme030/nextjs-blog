d7bb3587aad9ba888c884f2179edbfa4a7bb064e
treatme030 <treatme8192@gmail.com>
2022-08-02 19:44:13 +0900

    API 라우트로 데이터 전송

--- components/contact/contact_form.js
+++ components/contact/contact_form.js
@@ -1,23 +1,73 @@
+import { useState } from 'react';
 import classes from './contact_form.module.css';
 
 const ContactForm = () => {
+  const [contactInfo, setContactInfo] = useState({
+    email: '',
+    name: '',
+    message: '',
+  });
+
+  const { email, name, message } = contactInfo;
+
+  const changeInputed = (e) => {
+    const { name, value } = e.target;
+    setContactInfo({
+      ...contactInfo,
+      [name]: value,
+    });
+  };
+
+  const sendMessageHandler = (e) => {
+    e.preventDefault();
+
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify(contactInfo),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required />
+            <input
+              type='email'
+              id='email'
+              name='email'
+              required
+              value={email}
+              onChange={changeInputed}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' required />
+            <input
+              type='text'
+              id='name'
+              name='name'
+              required
+              value={name}
+              onChange={changeInputed}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor='message'>Your Message</label>
-          <textarea id='message' rows='5'></textarea>
+          <textarea
+            id='message'
+            rows='5'
+            name='message'
+            required
+            value={message}
+            onChange={changeInputed}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
