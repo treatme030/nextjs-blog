643bdf087ffb8f4582284cfe87a06a1e0fa5d32a
treatme030 <treatme8192@gmail.com>
2022-08-04 12:07:46 +0900

    syntax highlighter 코드 크기 줄이기

--- components/posts/post_detail/post_content.js
+++ components/posts/post_detail/post_content.js
@@ -1,10 +1,15 @@
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
 import classes from './post_content.module.css';
 import PostHeader from './post_header';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('css', css);
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
